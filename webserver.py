41a9ff243d782da7e3648dfd4bfff87617415078
Arpuraen <132832636+Arpuraen@users.noreply.github.com>
2023-05-28 19:34:19 +0700

    webserver.py

--- webserver.py
+++ webserver.py
@@ -16,16 +16,16 @@ content_types = {
 }
 
 while True:
-    connectionSocket, addr = serverSocket.accept()  # Membuat soket koneksi dan menerima request
+    connectionSocket, addr = serverSocket.accept()     # Membuat soket koneksi dan menerima request
 
     try:                                               # Mencoba menjalankan code yang berguna untuk mencari dan mengambil file dari file system
         # Menerima message dan mencari file yang diminta
         message = connectionSocket.recv(1024).decode() # Memasukkan paket ke yang sudah tiba kedalam variabel message dan mengkonversikan paketnya dari byte ke tipe data yang normal
         filename = message.split()[1]                  # Memasukkan value dengan indeks ke 1 dari message yang sudah displit kedalam variabel filename
-        f = open(filename[1:], 'rb')                   # Open file in binary mode for images
+        f = open(filename[1:], 'rb')                   # Membuka file dengan nama yang diambil dari variabel filename
         outputdata = f.read()                          # Membaca konten dari file f ke variabel outputdata
 
-        print("File found.")
+        print("File found.")                           # Mencetak pesan "File found."
         # Membuat header bahwa file telah ditemukan dan mengirim file
         extension = os.path.splitext(filename)[1][1:].lower()                           # Mengambil ekstensi file dari filename yang diberikan
         content_type = content_types.get(extension, "application/octet-stream")         # Menginisialisasi variabel content_type dengan tipe konten berdasarkan ekstensi file
